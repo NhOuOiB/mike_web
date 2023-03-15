a399c629e6c18673cc222ff86b11c883a362df63
NhOuOiB <s33165309000@gmail.com>
2023-03-15 10:34:02 +0800

    [feat] router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css'
-import './components/Todolist'
-import Todolist from './components/Todolist'
+import Home from './components/Home/Home'
+import TodoList from './components/TodoList/TodoList'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './components/Home/Nav/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <Todolist />
-    </div>
+    <>
+      <Nav />
+      <BrowserRouter basename={'/'}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/todolist" element={<TodoList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
