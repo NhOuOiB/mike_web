2be17f7f6f22f9b1401ba57678ceef33300650b4
NhOuOiB <s33165309000@gmail.com>
2023-03-15 09:30:23 +0800

    Move Home.js to src\components\Home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './_home.scss'
+const home = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-[18rem] h-[24rem] bg-gray-800 border-8 border-amber-200 rounded card"></div>
+      </div>
+    </>
+  )
+}
+
+export default home
