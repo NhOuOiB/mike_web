1fcce029ec214787da378b870b3b0fcd2ee28876
mike.chang <s33165309000@gmail.com>
2023-10-05 15:42:49 +0800

    [fix] basename

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css'
 import Home from './components/Home/Home'
 import TodoList from './components/TodoList/TodoList'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Nav from './components/Home/Nav/Nav'
 
 function App() {
   return (
     <>
       <Nav />
-      <BrowserRouter basename={'/mike_web'}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todolist" element={<TodoList />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todolist" element={<TodoList />} />
+      </Routes>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'antd/dist/antd.css'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter basename={'/mike_web'}>
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 )
 
